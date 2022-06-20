bdc41057bc28b2d990d5bf5e2ff82ce6900ce393
Guilherme Melo <jguilhermefmelo21@gmail.com>
2022-06-20 15:20:57 -0300

    adição da rota livros/buscar

--- src/routes/livrosRoutes.js
+++ src/routes/livrosRoutes.js
@@ -5,6 +5,7 @@ const routes = express.Router();
 
 routes.get("/livros", LivroController.listarLivros);
 routes.get("/livros/:id", LivroController.listaLivroPorId);
+routes.get("/livros/busca", LivroController.listarLivroPorEditora);
 routes.post("/livros", LivroController.cadastrarLivro);
 routes.put("/livros/:id", LivroController.atualizarLivro);
 routes.delete("/livros/:_id", LivroController.excluirLivro);
